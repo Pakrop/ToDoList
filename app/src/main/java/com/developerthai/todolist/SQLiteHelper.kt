2cee40faf6a171f0bc81d7298017e42eb0a03c8c
pakrop <pakrop.t@psru.ac.th>
2022-07-15 21:18:22 +0700

    first commit

---
+++ app/src/main/java/com/developerthai/todolist/SQLiteHelper.kt
@@ -0,0 +1,34 @@
+package com.developerthai.todolist
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class SQLiteHelper private constructor(context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
+    companion object {
+        private val DB_NAME = "mydb"
+        private val DB_VERSION = 1
+        private var mSqliteHelper: SQLiteHelper? = null
+
+        @Synchronized
+        fun getInstance(c: Context): SQLiteHelper {
+            return if (mSqliteHelper == null) {
+                SQLiteHelper(c.applicationContext)
+            } else {
+                mSqliteHelper!!
+            }
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        var sql = """CREATE TABLE todo (
+                            _id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            title TEXT,
+                            timestamp REAL)"""
+
+        db.execSQL(sql)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, v1: Int, v2: Int) {
+    }
+}
